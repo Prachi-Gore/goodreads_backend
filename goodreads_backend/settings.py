083d5e0609afd0dd00989ef69c398e20d4b4357f
Prachi Gore <prachigore408@gmail.com>
2025-03-08 16:02:27 +0530

    deployment chages

--- goodreads_backend/settings.py
+++ goodreads_backend/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import cloudinary
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()  # Load .env variables
 
@@ -97,16 +98,28 @@ WSGI_APPLICATION = 'goodreads_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-         "NAME": os.getenv("DB_NAME", "goodreads"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
-        'HOST': 'localhost',  # Or '127.0.0.1' 
-        'PORT': '5432'
+# Determine if running on Render
+IS_RENDER = "RENDER" in os.environ
+
+# Database Configuration
+if IS_RENDER:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME", "goodreads"),
+            "USER": os.getenv("DB_USER", "postgres"),
+            "PASSWORD": os.getenv("DB_PASSWORD", ""),
+            "HOST": os.getenv("DB_HOST", "localhost"),  # Default: local PostgreSQL
+            "PORT": os.getenv("DB_PORT", "5432"),       # Default PostgreSQL port
+        }
+    }
 
 
 
